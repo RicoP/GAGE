86f5685f18d438a4f19fb5b2ed4e63bb15c18343
RicoP <RicoP@users.noreply.github.com>
2024-06-07 10:49:34 +0200

    Remove unused files

--- source/download_video_example.bat
+++
@@ -1,1 +0,0 @@
-powershell -command "Invoke-WebRequest -Uri 'https://phoboslab.org/files/bjork-all-is-full-of-love.mpg' -OutFile 'bjork-all-is-full-of-love.mpeg'"
--- source/download_video_example.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget -O bjork-all-is-full-of-love.mpeg https://phoboslab.org/files/bjork-all-is-full-of-love.mpg 
