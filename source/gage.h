e5e08135a6268187053e5b83b48e6c9193c0fcc5
RicoP <RicoP@users.noreply.github.com>
2024-06-21 11:27:15 +0200

    refactor gage.h header

--- source/gage.h
+++ source/gage.h
@@ -21,18 +21,18 @@ bool choice(const char * text);
     return; \
     case __LINE__: {};
 
-#define CHOICE_END(...) if(!sm_did_choice) return; YIELD();
-
-#define CHOICE(TEXT) sm_did_choice = choice(TEXT) || sm_did_choice
-
-#define SCENE       scene
-#define SHOW        YIELD(); show
-#define SAY         YIELD(); say
-#define RETURN(...) for(;;) { YIELD(); }
-#define WAIT(TIME)  sm_wait_time = TIME;                \
-                    while(sm_wait_time > 0) {           \
-                        sm_wait_time -= GetFrameTime(); \
-                        sm_line = __LINE__;             \
-                        return;                         \
-                        case __LINE__: {};              \
-                    }
+#define CHOICE_END(...) do { if(!sm_did_choice) return; YIELD(); } while(0)
+#define CHOICE(TEXT)    sm_did_choice = choice(TEXT) || sm_did_choice
+#define SCENE(...)      do { scene(__VA_ARGS__); YIELD(); } while(0)
+#define SHOW(...)       do { show(__VA_ARGS__);  YIELD(); } while(0)
+#define SAY(...)        do { say(__VA_ARGS__);   YIELD(); } while(0)
+#define RETURN(...)     do { for(;;) { YIELD(); } } while(0)
+#define WAIT(TIME)      do {                                    \
+                            sm_wait_time = TIME;                \
+                            while(sm_wait_time > 0) {           \
+                                sm_wait_time -= GetFrameTime(); \
+                                sm_line = __LINE__;             \
+                                return;                         \
+                                case __LINE__: {};              \
+                            }                                   \
+                        } while(0)
